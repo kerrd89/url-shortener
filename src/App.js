94e34792327de9be0376107d3e66fb4de6e0c7fd
David Kerr <kerrd89@gmail.com>
2016-11-28 14:48:20 -0700

    GET function and created form for input field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './css/reset.css';
 import './css/App.css';
 
 class App extends Component {
+
+  getUrls(input) {
+    console.log('input', input);
+    axios.get(`/potato`)
+      .then((response) => {
+        console.log('response', response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
+    let input;
     return (
       <div className="App">
-        <div className="input-container">
-          <input type="text" placeholder="url"/>
-          <button>Submit</button>
-        </div>
+        <form
+          className="input-container"
+          onSubmit={ (e) => {
+            e.preventDefault();
+            this.getUrls(input.value);
+            input.value=('');
+          }}
+        >
+          <input
+            ref={ node => input = node}
+            type="text"
+            placeholder="url"
+          />
+          <button
+          className="submit-button">
+            Submit
+          </button>
+        </form>
+
         <div className="list-container">
           <ul>
-          <li>list here</li>
+            <li>list here</li>
           </ul>
         </div>
       </div>
