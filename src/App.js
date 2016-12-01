0457ea62b93861cf38eb03cfc5130f565d6beb91
David Kerr <kerrd89@gmail.com>
2016-12-01 11:23:37 -0700

    simplified the filtering there is only one button for ascending descending

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ class App extends Component {
 
   getUrls() {
     axios.get(`/urls`)
-      .then((response) => {
-        this.setState({urlList: response.data.urls});
+    .then((response) => {
+      this.setState({urlList: response.data.urls});
     })
     .catch((error) => {
       console.log(error);
@@ -36,30 +36,28 @@ class App extends Component {
     axios.post(`/post`, {
       url: input
     })
-      .then((response) => {
-        this.setState({displayShortUrl: "http://localhost:3001/api/" + response.data});
-        this.getUrls();
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setState({displayShortUrl: error});
-      });
+    .then((response) => {
+      this.setState({displayShortUrl: "http://localhost:3001/api/" + response.data});
+      this.getUrls();
+    })
+    .catch((error) => {
+      console.log(error);
+      this.setState({displayShortUrl: error});
+    });
   }
 
   filterUrls(searchValue) {
     let urls = this.state.urlList.filter(url => {
       if (url.longUrl.indexOf(searchValue.toLowerCase()) >= 0) {
-        console.log('filtered URL', url);
         return url;
       }
     });
     return urls;
-    // this.setState({ filteredUrls: urls });
   }
 
-  sortBy(property, direction) {
+  sortBy(property) {
     const { urlList } = this.state;
-    let sortedUrls = _.orderBy(urlList, [property], [direction]);
+    let sortedUrls = _.orderBy(urlList, [property]);
     this.setState({ urlList: sortedUrls });
   }
 
@@ -70,110 +68,111 @@ class App extends Component {
   render() {
     let input;
     let list;
-
-// TODO: ternary here to display regular list or filtered list?
-
     let urls = this.filterUrls(this.state.filterValue);
-    console.log(urls)
     if(urls.length){
       list = urls.map((url) => {
         return(
           <tr key={url.shortID}
-            className="url-row"
+          className="url-row"
           >
-            <td>{url.title}</td>
-            <td><a href={url.longUrl}>{url.longUrl}</a></td>
-            <td><a href={"http://localhost:3001/api/"+url.shortID}>{url.shortID}</a></td>
-            <td>{moment(url.createdAt).format('MMM Do, h:mm a')}</td>
-            <td>{url.count}</td>
+          <td>{url.title}</td>
+          <td><a href={url.longUrl}>{url.longUrl}</a></td>
+          <td><a href={"http://localhost:3001/api/"+url.shortID}>{url.shortID}</a></td>
+          <td>{moment(url.createdAt).format('MMM Do, h:mm a')}</td>
+          <td>{url.count}</td>
           </tr>
         )
       });
     }
+    if(!this.state.ascending) {list.reverse()}
     return (
       <div className="App">
-        <h1 className="title">Biggie/Smalls</h1>
-        <form
-          className="input-container"
-          onSubmit={ (e) => {
-            e.preventDefault();
-            this.postUrls(input.value);
-            input.value=('');
+      <h1 className="title">Biggie/Smalls</h1>
+      <form
+      className="input-container"
+      onSubmit={ (e) => {
+        e.preventDefault();
+        this.postUrls(input.value);
+        input.value=('');
+      }}
+      >
+      <input
+      ref={ node => input = node}
+      type="text"
+      placeholder="Type your url here"
+      className="input-field"
+      />
+      <button
+      className="submit-button">
+      Submit
+      </button>
+      </form>
+
+      <a href={this.state.displayShortUrl}>
+      <h1 className="short-url"
+      display={this.state.displayShortUrl}>
+      {this.state.displayShortUrl}
+      </h1>
+      </a>
+
+      <div className="list-container">
+        <section className="list-controls">
+        <input
+          type="text"
+          className="search-field"
+          placeholder="Search Long URLs"
+          onChange={(e) => this.setState({filterValue: e.target.value})}
+        />
+      <div className="pop-sort">
+
+        <button
+          onClick={() => {
+            this.sortBy('count')
+            this.setState({ascending: false})
+        }}
+          className="sort-button"
+          >
+          Popularity
+        </button>
+
+        <button
+          onClick={() => {
+            this.sortBy('createdAt')
+            this.setState({ascending: true})
           }}
-        >
-          <input
-            ref={ node => input = node}
-            type="text"
-            placeholder="Type your url here"
-            className="input-field"
-          />
-          <button
-            className="submit-button">
-            Submit
-          </button>
-        </form>
+          className="sort-button"
+          >
+          Date
+        </button>
 
-        <a href={this.state.displayShortUrl}>
-          <h1 className="short-url"
-            display={this.state.displayShortUrl}>
-            {this.state.displayShortUrl}
-          </h1>
-        </a>
+        <button
+          onClick={() => this.setState({ascending: !this.state.ascending})}
+          className="sort-button"
+          >
+          {this.state.ascending ? "⬆︎" : "⬇︎"}
+        </button>
 
-        <div className="list-container">
-          <section className="list-controls">
-            <input
-              type="text"
-              className="search-field"
-              placeholder="Search Long URLs"
-              onChange={(e) => this.setState({filterValue: e.target.value})}
-            />
-            <div className="pop-sort">
-              <button
-                onClick={() => this.sortBy('count', 'desc')}
-                className="sort-button"
-              >
-                Popularity ⬆︎
-              </button>
-              <button
-                onClick={() => this.sortBy('count', 'asc')}
-                className="sort-button"
-              >
-                Popularity ⬇︎
-              </button>
-            </div>
-            <div className="date-sort">
-              <button
-                onClick={() => this.sortBy('createdAt', 'desc')}
-                className="sort-button"
-              >
-                Date ⬆︎
-              </button>
-              <button
-                onClick={() => this.sortBy('createdAt', 'asc')}
-                className="sort-button"
-              >
-                Date ⬇︎
-              </button>
-            </div>
 
-          </section>
 
-          <table className="url-table">
-            <thead>
-              <tr>
-                <th className="table-headers">Title</th>
-                <th className="table-headers">Original URL</th>
-                <th className="table-headers">Short URL</th>
-                <th className="table-headers">Date Created</th>
-                <th className="table-headers">Popularity</th>
-              </tr>
-            </thead>
-            <tbody>
-              { list }
-            </tbody>
-          </table>
-        </div>
+      </div>
+
+      </section>
+
+      <table className="url-table">
+      <thead>
+      <tr>
+      <th className="table-headers">Title</th>
+      <th className="table-headers">Original URL</th>
+      <th className="table-headers">Short URL</th>
+      <th className="table-headers">Date Created</th>
+      <th className="table-headers">Popularity</th>
+      </tr>
+      </thead>
+      <tbody>
+      { list }
+      </tbody>
+      </table>
+      </div>
       </div>
     );
   }
